64dbcc90f265e9e6657b7472b3d6208ee6fcb291
manangouhari <gouhari21@hotmail.com>
2024-06-13 02:57:11 +0100

    🎨 Scaffolded contracts

---
+++ src/core/MarketFactory.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+/** 
+ * @title Contract to create new token markets. 
+ * @dev Responsible for accurately deploying new token markets with the preffered parameters. 
+ */
+contract MarketFactory {}
---
+++ src/core/MarketManager.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+/** 
+ * @title Contract to manage a token market
+ * @dev Orchestrates and stores the details for a market
+ */
+contract MarketManager {}
