18bf16010d6578e46ba50f57a377c350d517d32f
manangouhari <gouhari21@hotmail.com>
2024-06-15 12:47:22 +0100

    wip

--- src/core/MarketFactory.sol
+++ src/core/MarketFactory.sol
@@ -1,8 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-/** 
- * @title Contract to create new token markets. 
- * @dev Responsible for accurately deploying new token markets with the preffered parameters. 
+import {MarketManager} from "./MarketManager.sol";
+import {MarketCurve} from "./MarketCurve.sol";
+import {MarketToken} from "./MarketToken.sol";
+
+/**
+ * @title Contract to create new token markets.
+ * @dev Responsible for accurately deploying new token markets with the preffered parameters.
  */
-contract MarketFactory {}
+contract MarketFactory {
+    function createMarket(string calldata name, string calldata symbol) public {
+        // deploy a manager
+        // initialise the manager
+    }
+}
--- src/core/MarketManager.sol
+++ src/core/MarketManager.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-/** 
+/**
  * @title Contract to manage a token market
  * @dev Orchestrates and stores the details for a market
  */
