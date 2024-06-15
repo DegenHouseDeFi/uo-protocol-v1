b72fe6ef05f7e3f63238d8e4b8199af954da0b41
manangouhari <gouhari21@hotmail.com>
2024-06-15 18:40:50 +0100

    Comments for MarketParams + event on market creation

--- src/core/MarketFactory.sol
+++ src/core/MarketFactory.sol
@@ -11,17 +11,23 @@ import {MarketToken} from "./MarketToken.sol";
  * @dev Responsible for accurately deploying new token markets with the preffered parameters.
  */
 contract MarketFactory is Ownable {
-    mapping(MarketToken => MarketCurve) public tokenToCurve;
-
     struct MarketParameters {
-        uint256 liquidityCap;
-        uint256 xStartVirtualReserve;
-        uint256 yStartVirtualReserve;
-        uint256 yMintAmount;
-        uint256 yReservedForLP;
-        uint256 yReservedForCurve;
+        uint256 liquidityCap; // amount of ETH to be raised before moving to a DEX
+        uint256 xStartVirtualReserve; // initial virtual reserve for ETH
+        uint256 yStartVirtualReserve; // initial virtual reserve for created token
+        uint256 yMintAmount; // Supply of the created token
+        uint256 yReservedForLP; // Amount of created token to LP once curve cap is reached
+        uint256 yReservedForCurve; // Amount of created tokens to sell through the curve
     }
 
+    event MarketCreated(
+        address creator,
+        string name,
+        address token,
+        address curve
+    );
+
+    mapping(MarketToken => MarketCurve) public tokenToCurve;
     MarketParameters params;
 
     constructor(MarketParameters memory _params) Ownable(msg.sender) {
@@ -39,6 +45,8 @@ contract MarketFactory is Ownable {
         );
 
         tokenToCurve[token] = curve;
+
+        emit MarketCreated(msg.sender, name, token, curve);
     }
 
     function updateLiquidityCap(uint256 _liquidityCap) public onlyOwner {
