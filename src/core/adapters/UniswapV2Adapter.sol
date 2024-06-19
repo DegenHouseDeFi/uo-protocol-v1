1431daa6816628765a8a03b6a4b103e55ad3a14a
manangouhari <gouhari21@hotmail.com>
2024-06-19 03:36:56 +0100

    ✅ Fleshed out UniV2 liquidity adapter

--- src/core/adapters/UniswapV2Adapter.sol
+++ src/core/adapters/UniswapV2Adapter.sol
@@ -1,6 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import {IUniswapV2Factory} from "../../interfaces/uniswapV2/IUniswapV2Factory.sol";
+import {IUniswapV2Router02} from "../../interfaces/uniswapV2/IUniswapV2Router02.sol";
+
 contract UniswapV2LiquidityAdapter {
-    function addLiquidityETH() external payable {}
+    address public immutable WETH;
+    IUniswapV2Factory public immutable factory;
+    IUniswapV2Router02 public immutable router;
+
+    constructor(address _WETH, address _factory, address _router) {
+        WETH = _WETH;
+        factory = IUniswapV2Factory(_factory);
+        router = IUniswapV2Router02(_router);
+    }
+
+    function createPairAndAddLiquidityETH(address token, uint256 xToSupply, uint256 yToSupply, address to)
+        external
+        payable
+    {
+        require(msg.value >= xToSupply, "INSUFFICIENT_ETH");
+
+        // Create pair if it doesn't exist
+        address pair = IUniswapV2Factory(factory).getPair(token, WETH);
+        if (pair == address(0)) {
+            pair = IUniswapV2Factory(factory).createPair(token, WETH);
+        }
+
+        router.addLiquidityETH{value: xToSupply}(token, yToSupply, 1, 1, to, block.timestamp);
+    }
 }
