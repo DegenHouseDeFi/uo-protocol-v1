1f419e5e72762a62eb9ace8b34c156e702f1655c
manangouhari <gouhari21@hotmail.com>
2024-06-20 03:29:20 +0100

    🚧 [WIP] Integration Test for Base Mainnet

---
+++ test/chains/base-mainnet/Integration.t.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MarketFactory} from "../../../src/core/MarketFactory.sol";
+import {MarketToken} from "../../../src/core/MarketToken.sol";
+import {MarketCurve} from "../../../src/core/MarketCurve.sol";
+
+contract BaseIntegrationTest is Test {
+    MarketFactory public factory;
+
+    address constant WETH = address(0x4200000000000000000000000000000000000006);
+    address constant FACTORY = address(0x8909Dc15e40173Ff4699343b6eB8132c65e18eC6);
+    address constant ROUTER = address(0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24);
+
+    uint256 constant liquidityCap = 5.04 ether;
+    uint256 constant xStartVirtualReserve = 1.296 ether;
+    uint256 constant yStartVirtualReserve = 1_080_000_000 ether;
+    uint256 constant yMintAmount = 1_000_000_000 ether;
+    uint256 constant yReservedForLP = 200_000_000 ether;
+    uint256 constant yReservedForCurve = 800_000_000 ether;
+
+    receive() external payable {}
+
+    function setUp() public {
+        factory = new MarketFactory(
+            MarketFactory.MarketParameters({
+                liquidityCap: liquidityCap,
+                xStartVirtualReserve: xStartVirtualReserve,
+                yStartVirtualReserve: yStartVirtualReserve,
+                yMintAmount: yMintAmount,
+                yReservedForLP: yReservedForLP,
+                yReservedForCurve: yReservedForCurve
+            }),
+            WETH,
+            FACTORY,
+            ROUTER
+        );
+    }
+
+    function test_EndToEndFlow() public {
+        factory.createMarket("Test Token", "TT");
+
+        MarketToken token = MarketToken(factory.allTokens(0));
+        MarketCurve curve = MarketCurve(factory.tokenToCurve(token));
+
+        assertNotEq(address(token), address(0));
+        assertNotEq(address(curve), address(0));
+        assertEq(uint256(curve.status()), uint256(MarketCurve.Status.Trading));
+
+        uint256 amount = buyToken(curve, 1 ether);
+        sellToken(token, curve, amount / 2);
+
+        buyToken(curve, 5 ether);
+        assertEq(uint256(curve.status()), uint256(MarketCurve.Status.CapReached));
+    }
+
+    function buyToken(MarketCurve curve, uint256 amount) public returns (uint256) {
+        (uint256 xReserveBefore, uint256 yReserveBefore) = curve.getReserves();
+        (uint256 xBalanceBefore, uint256 yBalanceBefore) = curve.getBalances();
+        console.log("=====================================");
+        console.log("Buying token with amount: %d", amount);
+        console.log("Before: xReserve: %d, yReserve: %d", xReserveBefore, yReserveBefore);
+        console.log("Before: xBalance: %d, yBalance: %d", xBalanceBefore, yBalanceBefore);
+
+        uint256 tokensBought = curve.buy{value: amount}(amount);
+
+        (uint256 xReserveAfter, uint256 yReserveAfter) = curve.getReserves();
+        (uint256 xBalanceAfter, uint256 yBalanceAfter) = curve.getBalances();
+
+        console.log("-------------------------------------");
+        console.log("Tokens bought: %d", tokensBought);
+        console.log("After: xReserve: %d, yReserve: %d", xReserveAfter, yReserveAfter);
+        console.log("After: xBalance: %d, yBalance: %d", xBalanceAfter, yBalanceAfter);
+
+        assertEq(yBalanceBefore, yBalanceAfter + tokensBought);
+        assertEq(xBalanceAfter, xBalanceBefore + amount);
+
+        return tokensBought;
+    }
+
+    function sellToken(MarketToken token, MarketCurve curve, uint256 amount) public returns (uint256) {
+        (uint256 xReserveBefore, uint256 yReserveBefore) = curve.getReserves();
+        (uint256 xBalanceBefore, uint256 yBalanceBefore) = curve.getBalances();
+        console.log("=====================================");
+        console.log("Selling token with amount: %d", amount);
+        console.log("Before: xReserve: %d, yReserve: %d", xReserveBefore, yReserveBefore);
+        console.log("Before: xBalance: %d, yBalance: %d", xBalanceBefore, yBalanceBefore);
+
+        token.approve(address(curve), amount);
+        uint256 ethReceived = curve.sell(amount);
+
+        (uint256 xReserveAfter, uint256 yReserveAfter) = curve.getReserves();
+        (uint256 xBalanceAfter, uint256 yBalanceAfter) = curve.getBalances();
+
+        console.log("-------------------------------------");
+        console.log("Eth received: %d", ethReceived);
+        console.log("After: xReserve: %d, yReserve: %d", xReserveAfter, yReserveAfter);
+        console.log("After: xBalance: %d, yBalance: %d", xBalanceAfter, yBalanceAfter);
+
+        assertEq(yBalanceBefore + amount, yBalanceAfter);
+        assertEq(xBalanceBefore - ethReceived, xBalanceAfter);
+
+        return ethReceived;
+    }
+}
