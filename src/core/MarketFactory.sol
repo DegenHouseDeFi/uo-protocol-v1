59041caa384af82be825a33cd1c33b26465d5280
manangouhari <gouhari21@hotmail.com>
2024-06-28 01:25:18 +0100

    🎨 Custom Errors in MarketFactory

--- src/core/MarketFactory.sol
+++ src/core/MarketFactory.sol
@@ -12,6 +12,13 @@ import {UniswapV2LiquidityAdapter} from "./adapters/UniswapV2Adapter.sol";
  * @dev Responsible for accurately deploying new token markets with the preffered parameters.
  */
 contract MarketFactory is Ownable {
+    //////////////////// EVENTS ////////////////////
+    event MarketCreated(address creator, string name, address token, address curve);
+
+    //////////////////// ERRORS ////////////////////
+    error Factory_InvalidFee(uint256 expected, uint256 received);
+    error Factory_FailedEtherTransfer();
+
     //////////////////// DATA STRUCTURES ////////////////////
     struct MarketParameters {
         uint256 liquidityCap; // amount of ETH to be raised before moving to a DEX
@@ -29,9 +36,6 @@ contract MarketFactory is Ownable {
         uint256 tradeFee;
         uint256 graduationFee;
     }
-
-    //////////////////// EVENTS ////////////////////
-    event MarketCreated(address creator, string name, address token, address curve);
 
     //////////////////// VARIABLES ////////////////////
     address[] public allTokens;
@@ -55,6 +59,10 @@ contract MarketFactory is Ownable {
 
     //////////////////// FUNCTIONS ////////////////////
     function createMarket(string calldata name, string calldata symbol) public payable {
+        if (msg.value != feeParams.initiationFee) {
+            revert Factory_InvalidFee({expected: feeParams.initiationFee, received: msg.value});
+        }
+
         require(msg.value == feeParams.initiationFee, "INVALID_FEE");
 
         MarketCurve curve = new MarketCurve(
@@ -121,6 +129,8 @@ contract MarketFactory is Ownable {
 
     function sendEther(address to, uint256 amount) internal {
         (bool sent,) = to.call{value: amount}("");
-        require(sent, "Failed to send Ether");
+        if (!sent) {
+            revert Factory_FailedEtherTransfer();
+        }
     }
 }
