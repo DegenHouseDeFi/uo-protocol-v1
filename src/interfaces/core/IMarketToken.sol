18483b51a7b7e3359d7471e49beb4e68a26ad20a
manangouhari <gouhari21@hotmail.com>
2024-07-05 02:22:47 +0530

    Updated MarketToken Interface

--- src/interfaces/core/IMarketToken.sol
+++ src/interfaces/core/IMarketToken.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.8.13;
 
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
-interface IMarketToken is IERC20 {}
+interface IMarketToken is IERC20 {
+    function setGraduated(bool _isGraduated) external;
+}
